7bf421cb22cb182c8ce0f1a834ee5202a1ff0761
Shyam Sunder <sgsunder1@gmail.com>
2018-01-03 11:24:19 -0500

    Moved Discord API Token to Seperate File

--- settings.py
+++ settings.py
@@ -1,32 +1,46 @@
 import json
 
+# Simple class to store settings, uses JSON file for permanance
 class Settings:
 	def __init__(self):
 		self._path = "settings.json"
+		self._tokenpath = "token"
 		self._settings = self._load_settings()
-		
+
+		self.token = self._load_token()
+
 		self.admin_role = self._settings["admin role"]
 		self.mod_role = self._settings["mod role"]
 		self.prefix = self._settings["prefix"]
 		self.roles = self._settings["roles"]
-		self.token = self._settings["token"]
-	
+
+	# Loads Class Variables from JSON file
 	def _load_settings(self):
 		file = open(self._path, "r")
-		settings = json.loads(file.read())
+		data = json.loads(file.read())
 		file.close()
-		
-		return settings
-	
+
+		return data
+
+	# Dumps Current Class Variables to JSON file
 	def _save_settings(self):
 		settings_string = json.dumps(self._settings, sort_keys=True, indent=4, separators=(",", ": "))
 		file = open(self._path, "w")
 		file.write(settings_string)
 		file.close()
-	
+
+	# Changes a setting and updates JSON file
 	def change_setting(self, setting, new_setting):
 		self._settings[setting] = new_setting
 		self._save_settings()
 		self._load_settings()
-	
+
+	# Loads the Discord API Token from a seperate file
+	def _load_token(self):
+		file = open(self._tokenpath, "r")
+		data = file.read().replace('\n', '')
+		file.close()
+
+		return data
+
 settings = Settings()
